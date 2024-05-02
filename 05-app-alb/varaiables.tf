bea4e306eb15557f0004ba918acc65d8b1046dec
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-05-02 17:38:47 +0530

    app_alb

---
+++ 05-app-alb/varaiables.tf
@@ -0,0 +1,32 @@
+variable "common_tags" {
+  default = {
+    project_name = "roboshop"
+    environment = "dev"
+    terraform = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    component = "app-alb"
+  }
+}
+variable "project_name" {
+    type = string
+    default = "ROBOSHOP"
+  
+}
+
+variable "environment" {
+  default = "DEV"
+}
+
+# variable "instance_type" {
+#   type = string
+#   default = "t3.small"
+# }
+
+
+variable "zone_name" {
+  default = "bigmatrix.in"
+}
