af253a634cec44af8c5b972d8dff316a4e78c162
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-05-02 17:36:24 +0530

    new sg

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -133,7 +133,7 @@ resource "aws_security_group_rule" "app_alb_vpn" { #vpn accepting from home
   type              = "ingress"
   from_port         = 80
   to_port           = 80
-  protocol          = "-1"
+  protocol          = "tcp"
   security_group_id = module.app_alb.sg_id
 }
 
@@ -258,7 +258,14 @@ resource "aws_security_group_rule" "catalogue_vpn" {
   protocol                 = "tcp"
   security_group_id        = module.catalogue.sg_id
 }
-
+resource "aws_security_group_rule" "catalogue_vpn_http" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.catalogue.sg_id
+}
 # resource "aws_security_group_rule" "catalogue_web" {
 #   source_security_group_id = module.web.sg_id
 #   type                     = "ingress"
