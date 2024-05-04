4c318a26f997b9530147f28b0838cb28d852ea89
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-05-04 12:24:40 +0530

    cdn

---
+++ 10-cdn/data.tf
@@ -0,0 +1,9 @@
+# dns certificate 
+data "aws_ssm_parameter" "web_alb_dns_name" {
+  name = "/${var.project_name}/${var.environment}/web_alb_dns_name"
+}
+
+# certificate 
+data "aws_ssm_parameter" "acm_certificate_arn" {
+  name = "/${var.project_name}/${var.environment}/acm_certificate_arn"
+}
